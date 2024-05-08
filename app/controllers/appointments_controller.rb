bb7c176199552bdd3350c29325a8461e256d53c7
Oleh Collins <oleh.collins@icloud.com>
2024-05-08 08:41:15 +0100

    added styling, some additional routes and stimulus controllers to toggle forms

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,44 @@
 class AppointmentsController < ApplicationController
+  before_action :set_consultation, only: [:new, :create]
+
+  def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.new()
+    @appointment.status = appointment_params[:status]
+    @appointment.consultation = @consultation
+
+    full_name = appointment_params[:user].split
+    patient = User.find_by(first_name: full_name[0], last_name: full_name[1])
+
+    if patient
+      @appointment.user_id = patient.id
+    else
+      redirect_to new_consultation_appointment_path(@consultation), notice: "no such patient exist"
+      return
+    end
+    # raise
+    if @appointment.save
+      redirect_to consultation_path(@consultation), notice: "Appointment was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Appointment.find(params[:id]).destroy
+    redirect_to consultations_path, notice: "Appointment was successfully deleted."
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:status,:user)
+  end
+
+  def set_consultation
+    @consultation = Consultation.find(params[:consultation_id])
+  end
 end
--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,26 +1,43 @@
 class ConsultationsController < ApplicationController
+  before_action :set_consultation, only: [:show, :destroy]
+
   def index
-    # @consultations = current_user.consultations
-    @consultations = Consultation.all
+    @consultation = Consultation.new
+    @consultations = current_user.consultations
   end
+
   def show
-    @consultation = Consultation.find(params[:id])
+    @appointments = @consultation.appointments
+    @appointment = Appointment.new
   end
+
   def new
     @consultation = Consultation.new
   end
+
   def create
     @consultation = Consultation.new(consultation_params)
     @consultation.user = current_user
+
     if @consultation.save
-    redirect_to consultation_path(@consultation)
+      redirect_to consultation_path(@consultation), notice: "Consultaion was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def destroy
+    @consultation.destroy
+    redirect_to consultations_path, notice: "Consultaion was successfully deleted."
+  end
+
   private
+
   def consultation_params
     params.require(:consultation).permit(:price_per_hour,:languages_spoken,:pretty_title,:location_city,:speciality,:years_of_experience,:availability,:duration)
+  end
+
+  def set_consultation
+    @consultation = Consultation.find(params[:id])
   end
 end
