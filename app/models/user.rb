561c036c24bbd1e9f5607f0ce6053b155e65f5b0
Oleh Collins <oleh.collins@icloud.com>
2024-05-08 12:00:08 +0100

    reordered migration file to avoid crashed during deployment

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :consultations, dependent: :destroy
   has_many :appointments, through: :consultations
+  has_many :appointments
 
   validates :first_name, presence: true
   validates :last_name, presence: true
