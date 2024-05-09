444f4df43ed7fb59b83423c4c61d66c6b1df53d8
Oleh Collins <oleh.collins@icloud.com>
2024-05-09 12:45:32 +0100

    the user can now upload their image and each consultation as its own image

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,5 @@ class PagesController < ApplicationController
   def home
     @appointments = Appointment.where(user_id: current_user)
     @consultations = Consultation.all
-    @user = current_user
   end
 end
